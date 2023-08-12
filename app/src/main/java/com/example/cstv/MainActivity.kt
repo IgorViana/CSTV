cfb97acf859df92dab5ff78f9302d781f4be6fa0
Igor <igorviana40@hotmail.com>
2023-08-12 20:28:35 -0300

    Implementing splash screen

--- app/src/main/java/com/example/cstv/MainActivity.kt
+++ app/src/main/java/com/example/cstv/MainActivity.kt
@@ -13,7 +13,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.example.cstv.ui.navigation.Navigation
 import com.example.cstv.ui.theme.CSTVTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/cstv/ui/screen/splash/SplashScreen.kt
+++ app/src/main/java/com/example/cstv/ui/screen/splash/SplashScreen.kt
@@ -1,15 +1,53 @@
 package com.example.cstv.ui.screen.splash
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.cstv.R
+import com.example.cstv.ui.navigation.NavigationScreens
 import com.example.cstv.ui.theme.CSTVTheme
 
 @Composable
 fun SplashScreen(navController: NavController) {
-    Text(text = "Splash")
+
+    val viewModel: SplashViewModel = viewModel()
+
+    val isLoading = viewModel.isLoading.collectAsState()
+
+    if (!isLoading.value) {
+        navController.navigate(NavigationScreens.MainScreen.name)
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Blue),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            modifier = Modifier
+                .width(113.dp)
+                .height(113.dp),
+            painter = painterResource(id = R.drawable.fuze_logo),
+            contentDescription = "fuze logo",
+            contentScale = ContentScale.FillBounds
+        )
+    }
 }
 
 @Preview(showBackground = true)
